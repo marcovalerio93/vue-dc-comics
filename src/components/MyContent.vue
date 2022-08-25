978e10d2b24d62cf9173f1cfef50e38e018d5874
Marco Valerio <mo.valerio93@gmal.com>
2022-08-25 16:36:59 +0200

    vue-dc structure + imgs

---
+++ src/components/MyContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:'MyContent',
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,10 +1,21 @@
 <template>
-  
-</template>
+    <div>
+        <header>
+    <img src="@/assets/img/">
+    <nav>
+        <ul>
+            <li v-for="(link, index) in pincopallino" :key="index" >
+                <a :href="link.url" :class="link.active?'active':''" >{{link.text}}</a>
+            </li>
+        </ul>
+    </nav>
+  </header>
+    </div>
+</template> 
 
 <script>
 export default {
-
+    name:'MyHeader',
 }
 </script>
 
